907ec6c8aafe73ba201186765cfb21718f4e0026
timtiph <tiphany.moine@outlook.com>
2022-12-06 15:28:52 +0100

    first commit

---
+++ config/app.php
@@ -0,0 +1,9 @@
+<?php
+
+declare (strict_types = 1);
+
+return [
+    'env' => env('APP_ENV', 'production'),
+    'timezone' => 'Europe/Paris',
+    
+];
---
+++ config/twig.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    
+    'template_extension' => 'html',
+    'functions' => [
+        'auth', 'route', 'errors', 'status',
+        'csrf_field', 'method', 'old',
+    ],
+];
