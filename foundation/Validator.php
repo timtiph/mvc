a9324b1725a0e429ff4f4dff553ca36b4a35ae80
timtiph <tiphany.moine@outlook.com>
2022-12-06 16:56:47 +0100

    Ajout regle VALIDATOR

--- foundation/Validator.php
+++ foundation/Validator.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Tmoi\Foundation;
 
-
+use Illuminate\Database\Capsule\Manager as Capsule;
 use Valitron\Validator as ValitronValidator;
 
 class Validator 
@@ -22,6 +22,36 @@ class Validator
 
     protected static function addCustomRules(ValitronValidator $validator): void
     {
-        // Custom rules here
+        // Custom rules here ;
+
+        // $validator->addRule('unique', function (string $field, mixed $value, array $params, array $fields) {
+        //     return !Capsule::table($params[1])->where($params[0], $value)->exists();
+        // }, '{field} est invalide');
+
+        // $validator->addRule('password', function (string $field, mixed $value, array $params, array $fields) {
+        //     $user = Authentication::get();
+        //     return password_verify($value, $user->password);
+        // }, '{field} est erroné');
+
+        // $validator->addRule('required_file', function (string $field, mixed $value, array $params, array $fields) {
+        //     return isset($_FILES[$field]) && $_FILES[$field]['error'] === UPLOAD_ERR_OK;
+        // }, '{field} est obligatoire');
+
+        // $validator->addRule('image', function (string $field, mixed $value, array $params, array $fields) {
+        //     if (isset($_FILES[$field]) && $_FILES[$field]['error'] === UPLOAD_ERR_OK) {
+        //         return str_starts_with($_FILES[$field]['type'], 'image/');
+        //     }
+        //     return false;
+        // }, '{field} doit être une image');
+
+        // $validator->addRule('square', function (string $field, mixed $value, array $params, array $fields) {
+        //     if (isset($_FILES[$field]) && $_FILES[$field]['error'] === UPLOAD_ERR_OK) {
+        //         [$width, $height] = getimagesize($_FILES[$field]['tmp_name']);
+        //         return $width === $height;
+        //     }
+        //     return false;
+        // }, '{field} doit être carré (hauteur = largeur)');
+
+
     }
 }
